90054d0521579d0b0aab7aee8b888d5949a70ce1
amurikurthy <59367704+amurikurthy@users.noreply.github.com>
2024-08-09 14:26:53 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,11 +1,21 @@
 import requests
+import pandas as pd
+
+
+def divide_pid_chunks(lst, n):
+    # Loop to divide into chunks of size n
+    for i in range(0, len(lst), n):
+        yield ','.join(lst[i:i + n])
+
+unique_child_pid_df = pd.read_excel('Cisco Child PID.xlsx')["Child PID"].unique()
+
+pid_chunks = list(divide_pid_chunks(unique_child_pid_df, 20))
 
 # Token endpoint provided by Cisco
 url = 'https://id.cisco.com/oauth2/default/v1/token'
 
 # Client ID and Client Secret
-client_id = ''
-client_secret = ''
+
 
 # Body parameters for the token request
 body = {
@@ -26,8 +36,6 @@ response = requests.post(url, data=body, headers=headers)
 if response.status_code == 200:
     access_token = response.json()['access_token']
     expires_in = response.json()['expires_in']
-    print("Access Token:", access_token)
-    print("Expires In:", expires_in, "seconds")
 else:
     print("Failed to retrieve access token:", response.status_code, response.text)
 
@@ -38,25 +46,33 @@ api_key = response.json()['access_token']
 # Endpoint URL for EoX API
 url = 'https://apix.cisco.com/supporttools/eox/rest/5/EOXByProductID/1/{product_id}'
 
-# Replace '{product_id}' with the actual product ID you want to query
-product_id = ''
-formatted_url = url.format(product_id=product_id)
+eox_pids = []
 
-# Headers to include in the request
-headers = {
-    'Authorization': f'Bearer {api_key}',
-    'Content-Type': 'application/json'
-}
+for pid_chunk in pid_chunks:
 
-# Make the GET request
-response = requests.get(formatted_url, headers=headers)
+    # Replace '{product_id}' with the actual product ID you want to query
+    product_id = pid_chunk
+    formatted_url = url.format(product_id=product_id)
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Process the response
-    eox_data = response.json()
-    print(eox_data)
-else:
-    # Handle request error
-    print('Failed to retrieve data:', response.status_code)
+    # Headers to include in the request
+    headers = {
+        'Authorization': f'Bearer {api_key}',
+        'Content-Type': 'application/json'
+    }
 
+    # Make the GET request
+    response = requests.get(formatted_url, headers=headers)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Process the response
+        eox_data = response.json()
+        for EOLProduct in eox_data["EOXRecord"]:
+            if EOLProduct["EOLProductID"]:
+                eox_pids.append(EOLProduct)
+
+    else:
+        # Handle request error
+        print('Failed to retrieve data:', response.status_code)
+
+eox_pids_df = pd.DataFrame(eox_pids)
